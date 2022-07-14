ee17b849cd91c24d28e1317a525dd71b5ee32839
RagingBigDog <59346437+RagingBigDog@users.noreply.github.com>
2022-07-14 02:44:29 -0500

    Found out there was a discord.py 2.0 Installed that and followed a tutorial to get the bot setup using version 2.0

--- bot.py
+++ bot.py
@@ -1,36 +1,18 @@
 import discord
+from discord.ext import commands
 import os
-import random
 from dotenv import load_dotenv
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 token = os.getenv('TOKEN')
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print('Logged in as a bot {0.user}'.format(client))
+    print('Bot is live!')
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split('#')[0]
-    channel = str(message.channel.name)
-    user_messsage = str(message.content)
+@bot.command()
+async def ping(ctx):
+    await ctx.reply('pong')
 
-    print(f'Message {user_messsage} by {username} on {channel}')
-
-    if message.author == client.user:
-        return
-    
-    if channel == 'random':
-        if user_messsage.lower() == 'hello' or user_messsage.lower() == 'hi':
-            await message.channel.send(f'Hello {username}')
-            return
-        elif user_messsage.lower() == 'bye':
-            await message.channel.send(f'Bye {username}')
-        elif user_messsage.lower() == 'tell me a joke':
-            jokes = [' Can someone please shed more\
-                light on how my lamp got stolen?', 
-                'Why is she calle Ilene? She \
-                stands on equal legs.', 'What do you call a gazelle in a \
-                lions terriorty? Denzel.']
-            await message.channel.send(random.choice(jokes))
+bot.run(token)
