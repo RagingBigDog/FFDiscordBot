45e034a8b7079493f60891cf0857c5a1654ebf26
RagingBigDog <59346437+RagingBigDog@users.noreply.github.com>
2022-07-15 03:23:42 -0500

    Added command to create channel

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from dis import disco
 import discord
 from discord.ext import commands
 import os
@@ -12,7 +13,19 @@ async def on_ready():
     print('Bot is live!')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.reply('pong')
+async def create_channel(ctx):
+    for channel in ctx.guild.text_channels:
+        if channel.name == str(ctx.author.id):
+            return await ctx.send(embed=discord.Embed(title='Failure!', description=f'Channel <#{channel.id}> already created!', color=discord.Color.red()))
+    guild = ctx.guild
+    member = ctx.author
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        guild.me: discord.PermissionOverwrite(read_messages=True),
+        member: discord.PermissionOverwrite(read_messages=True)
+    }
+
+    channel = await ctx.guild.create_text_channel(str(ctx.author.id), overwrites=overwrites)
+    return await ctx.send(embed=discord.Embed(title='Success!', description=f'Channel <#{channel.id}> created!', color=discord.Color.green()))
 
 bot.run(token)
