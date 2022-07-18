ee9c15cc51461e53f7f32ecf89844dfde41db597
RagingBigDog <59346437+RagingBigDog@users.noreply.github.com>
2022-07-18 03:50:14 -0500

    Added bidCommand and load_env()*
    *not sure how the bot was running before, unless it was deleted

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import json
 
+load_dotenv()
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 token = os.getenv('TOKEN')
 
@@ -27,5 +29,25 @@ async def create_channel(ctx):
 
     channel = await ctx.guild.create_text_channel(str(ctx.author.id), overwrites=overwrites)
     return await ctx.send(embed=discord.Embed(title='Success!', description=f'Channel <#{channel.id}> created!', color=discord.Color.green()))
+
+@bot.command()
+async def bid(ctx, amount:int, *, player:str):
+    name = str(ctx.author.id)
+    if name in ctx.guild.text_channels:
+        await ctx.message.delete()
+        raise commands.PrivateMessageOnly('You can\'t run this command outside a private channel The command you typed is being deleted for secrecy')
+    with open('data.json', 'r') as data_file:
+        data = json.loads(data_file.read() or '{}')
+        oldamount = 0
+        if not str(ctx.author.id) in data:
+            data[str(ctx.author.id)] = {player.lower(): amount}
+        else:
+            oldamount = data[str(ctx.author.id)].get(player.lower()) or 0
+            data[str(ctx.author.id)].update({player.lower(): amount})
+        data_file.close()
+    with open('data.json', 'w') as data_file:
+        data_file.write(json.dumps(data, indent=4))
+        data_file.close()
+    return await ctx.send(embed=discord.Embed(title='Success!', description=f"Changed your bid of {player} from {oldamount} to {amount}", color=discord.Color.green()))
 
 bot.run(token)
