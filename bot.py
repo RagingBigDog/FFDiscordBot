b4be7697b20be5339bcf66d9e316129b39cb73da
RagingBigDog <59346437+RagingBigDog@users.noreply.github.com>
2022-07-17 23:34:54 -0500

    Updated CreateChannelCommand

--- bot.py
+++ bot.py
@@ -18,11 +18,11 @@ async def create_channel(ctx):
         if channel.name == str(ctx.author.id):
             return await ctx.send(embed=discord.Embed(title='Failure!', description=f'Channel <#{channel.id}> already created!', color=discord.Color.red()))
     guild = ctx.guild
-    member = ctx.author
+    author = ctx.author
     overwrites = {
         guild.default_role: discord.PermissionOverwrite(read_messages=False),
         guild.me: discord.PermissionOverwrite(read_messages=True),
-        member: discord.PermissionOverwrite(read_messages=True)
+        author: discord.PermissionOverwrite(read_messages=True)
     }
 
     channel = await ctx.guild.create_text_channel(str(ctx.author.id), overwrites=overwrites)
