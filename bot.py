5e2ff6d48ad46dba9f6a7cf39d87d223b118e69e
RagingBigDog <59346437+RagingBigDog@users.noreply.github.com>
2022-07-19 03:33:35 -0500

    Added view bids command and added message if called in wrong channel*
    *Still need to work on deleting the original message if in wrong channel

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+from dis import dis
+import string
 from urllib import response
 import discord
 from discord.ext import commands
-from discord import app_commands
+from discord import InteractionMessage, app_commands
 import os
 from dotenv import load_dotenv
 import json
@@ -43,20 +45,46 @@ async def self(interaction: discord.Interaction):
 @tree.command(name='place_bid', description='place a bid on a player', guild=discord.Object(id=653484637934321674))
 async def self(interaction: discord.Interaction, bid: int, player: str):
     name = str(interaction.user.id)
-    if name in interaction.guild.text_channels:
-        raise commands.PrivateMessageOnly('You can\'t run this command outside a private channel!')
-    with open('data.json', 'r') as data_file:
-        data = json.loads(data_file.read() or '{}')
-        oldamount = 0
-        if not str(interaction.user.id) in data:
-            data[str(interaction.user.id)] = {player.lower(): bid}
-        else:
-            oldamount = data[str(interaction.user.id)].get(player.lower()) or 0
-            data[str(interaction.user.id)].update({player.lower(): bid})
-        data_file.close()
-    with open('data.json', 'w') as data_file:
-        data_file.write(json.dumps(data, indent=4))
-        data_file.close()
-    return await interaction.response.send_message(embed=discord.Embed(title='Success!', description=f"Changed your bid of {player} from {oldamount} to {bid}", color=discord.Color.green())) 
+    if name != interaction.channel.id :
+        await interaction.response.send_message(f'You can\'t run this command outside your private channel')
+        raise app_commands.NoPrivateMessage('You can\'t run this command outside a private channel!')
+    else:
+        with open('data.json', 'r') as data_file:
+            data = json.loads(data_file.read() or '{}')
+            oldamount = 0
+            if not str(interaction.user.id) in data:
+                data[str(interaction.user.id)] = {player.lower(): bid}
+            else:
+                oldamount = data[str(interaction.user.id)].get(player.lower()) or 0
+                data[str(interaction.user.id)].update({player.lower(): bid})
+            data_file.close()
+        with open('data.json', 'w') as data_file:
+            data_file.write(json.dumps(data, indent=4))
+            data_file.close()
+        return await interaction.response.send_message(embed=discord.Embed(title='Success!', description=f"Changed your bid of {player} from {oldamount} to {bid}", color=discord.Color.green())) 
+
+@tree.command(name='view_bids', description='view your current bids', guild=discord.Object(id=653484637934321674))
+async def self(interaction: discord.Interaction):
+    name = str(interaction.user.id)
+    string = ''
+    if name != interaction.channel.name:
+        await interaction.response.send_message(f'You can\'t run this command outside your private channel')
+        raise app_commands.NoPrivateMessage('You can\'t run this command outside a private channel!')
+    else:
+        with open('data.json', 'r') as data_file:
+            data = json.loads(data_file.read() or '{}')
+            mdata = data.get(str(interaction.user.id)) or None
+            if mdata:
+                for k in dict(mdata):
+                    string += f'`{k}`: `{dict(mdata)[k]}`\n'
+            else:
+                return await interaction.response.send_message(f'You have no bids!')
+            
+            n = 4000
+            strings = [string[i:i+n] for i in range(0, len(string), n)]
+
+            for s in strings:
+                await interaction.response.send_message(s)
+    
 
 slashBot.run(token)
